e630fbfdb8531b7c76f5724423c792d358853821
Jean Brito <otirbnaej@hotmail.com>
2018-01-03 17:12:00 -0300

    alguns modelos adicionados no admin

--- sistema/crud/admin.py
+++ sistema/crud/admin.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 from crud.models import (
     CourseGrid,
     Activity,
     Title,
     ContractType,
+    Campus,
+    Area,
+    Teacher,
+    Course,
+    Discipline,
 )
 
 admin.site.register(CourseGrid)
 admin.site.register(Activity)
 admin.site.register(Title)
 admin.site.register(ContractType)
+admin.site.register(Campus)
+admin.site.register(Area)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Discipline)
 
