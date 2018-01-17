a3d9996ac96e8ae2b8bf168bec1663ae3485b840
Luiz <luizrodrigo46@hotmail.com>
2018-01-17 17:56:18 -0300

    create Campus with modelform

--- sistema/crud/views.py
+++ sistema/crud/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from crud.forms import CampusForm
 
-# Create your views here.
+def create_campus(request):
+    context = {}
+    if request.method == 'POST':
+        form = CampusForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = CampusForm()
+
+    context = {'form': form}
+
+    return render(request, 'campus/create.html', {'form':form})
--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from crud import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^create/campus/', views.create_campus, name='create_campus'),
 ]
