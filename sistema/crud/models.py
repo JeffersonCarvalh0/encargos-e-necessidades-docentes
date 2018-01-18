5c0187f13958f4abbc54738174b036d822eaefd8
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-18 10:25:09 -0300

    Nomes em português adicionados nos modelos.
    Nomes em português adicionados para o campus
    
    Nomes em português adicionados para o curso
    
    Tinha esquecido de adicionar alguns nomes no curso
    
    Adicionados nomes em português para o curso
    
    Adicionados nomes em português para a área
    
    Adicionados nomes em português para as disciplinas
    
    Adicionados nomes em português para as titulações
    
    Adicionados nomes em português para os tipos de contrato
    
    Adicionados nomes em português para as naturezas das atividades
    
    Adicionados nomes em português para os tipos de atividades
    
    Adicionados nomes em português para os professores
    
    Adicionados nomes em português para as atividades

--- sistema/crud/models.py
+++ sistema/crud/models.py
@@ -7,19 +7,19 @@ from django.db import models
 # Campus
 #
 class Campus(models.Model):
-    name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=30)
-    add_street = models.CharField(max_length=50)
-    add_no = models.IntegerField()
-    add_neighbor = models.CharField(max_length=50)
-    add_city = models.CharField(max_length=50)
-    add_uf = models.CharField(max_length=2)
-    add_zip = models.CharField(max_length=50)
-    phone1 = models.CharField(max_length=14)
-    phone2 = models.CharField(max_length=14)
-    email = models.EmailField()
+    name = models.CharField('nome', max_length=50)
+    short_name = models.CharField('nome curto', max_length=30)
+    add_street = models.CharField('rua', max_length=50)
+    add_no = models.IntegerField('número')
+    add_neighbor = models.CharField('bairro', max_length=50)
+    add_city = models.CharField('cidade', max_length=50)
+    add_uf = models.CharField('estado', max_length=2)
+    add_zip = models.CharField('CEP', max_length=50)
+    phone1 = models.CharField('telefone 1', max_length=14)
+    phone2 = models.CharField('telefone 2', max_length=14)
+    email = models.EmailField('e-mail')
     site = models.URLField()
-    active = models.BooleanField()
+    active = models.BooleanField('ativo')
 
     def __unicode__(self):
         return self.short_nome
@@ -31,155 +31,167 @@ class Campus(models.Model):
 # Curso
 #
 class Course(models.Model):
-    name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=20)
+    name = models.CharField('nome', max_length=50)
+    short_name = models.CharField('nome curto', max_length=20)
     campus = models.ForeignKey(Campus)
-    active = models.BooleanField()
+    active = models.BooleanField('ativo')
 
     def __unicode__(self):
         return self.nome
     class Meta:
-        verbose_name_plural = "Courses"
+        verbose_name = 'curso'
+        verbose_name_plural = "cursos"
 
 #
 # Matriz
 #
 class CourseGrid(models.Model):
-    name = models.CharField(max_length=50)
-    course = models.ForeignKey(Course)
-    date_ini = models.DateField(null=True)
-    date_term = models.DateField(null=True)
-    active = models.BooleanField()
+    name = models.CharField('nome', max_length=50)
+    course = models.ForeignKey('curso', Course)
+    date_ini = models.DateField('data início', null=True)
+    date_term = models.DateField('data término', null=True)
+    active = models.BooleanField('ativo')
 
     def __unicode__(self):
         return self.nome
 
     class Meta:
-        verbose_name_plural = "CourseGrids"
+        verbose_name = 'Grade curricular'
+        verbose_name_plural = "Grades curriculares"
 
 #
 # Area
 #
 class Area(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('nome', max_length=50)
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = "Areas"
+        verbose_name = 'Área'
+        verbose_name_plural = "Áreas"
 
 #
 # Disciplina
 #
 class Discipline(models.Model):
-    name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=20)
-    grid = models.ForeignKey(CourseGrid)
-    area = models.ForeignKey(Area)
+    name = models.CharField('nome', max_length=50)
+    short_name = models.CharField('nome curto', max_length=20)
+    grid = models.ForeignKey('grade curricular', CourseGrid)
+    area = models.ForeignKey('área', Area)
     ementa = models.TextField()
-    block = models.SmallIntegerField()
-    work_load = models.SmallIntegerField()
+    block = models.SmallIntegerField('bloco')
+    work_load = models.SmallIntegerField('carga horária')
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = "Disciplines"
+        verbose_name = 'Disciplina'
+        verbose_name_plural = "Disciplinas"
 
 
 #
 # Titulação
 #
 class Title(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('nome', max_length=50)
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = "Titles"
+        verbose_name = 'Titulação'
+        verbose_name_plural = "Titulações"
 
 #
 # TipoContrato
 #
 class ContractType(models.Model):
-    name = models.CharField(max_length=50)
-    wk_teaching = models.IntegerField()
-    wk_resext = models.IntegerField()
-    wk_compl = models.IntegerField()
-    active = models.BooleanField()
+    name = models.CharField('nome', max_length=50)
+    wk_teaching = models.IntegerField('carga horária de ensino')
+    wk_resext = models.IntegerField('carga horária de extensão')
+    wk_compl = models.IntegerField('carga horária complementar')
+    active = models.BooleanField('ativo')
 
     def __unicode__(self):
         return self.nome
 
     class Meta:
-        verbose_name_plural = "Contract Types"
+        verbose_name = 'Tipo de contrato'
+        verbose_name_plural = "Tipos de contrato"
 
 #
 # NaturezaAtividade
 #
 class ActivityNature(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('nome', max_length=50)
 
     def __unicode__(self):
         return self.nome
 
     class Meta:
-        verbose_name_plural = "Activity Natures"
+        verbose_name: 'Natureza da atividade'
+        verbose_name_plural = "Naturezas de atividades"
 
 #
 # TipoAtividade
 #
 class ActivityType(models.Model):
-    name = models.CharField(max_length=50)
-    wk_week = models.IntegerField()
-    wk_limit = models.IntegerField()
-    nature = models.ForeignKey(ActivityNature)
+    name = models.CharField('nome', max_length=50)
+    wk_week = models.IntegerField('carga horária semanal')
+    wk_limit = models.IntegerField('carga horária limite')
+    nature = models.ForeignKey('natureza', ActivityNature)
 
     def __unicode__(self):
         return self.nome
+
     class Meta:
-        verbose_name_plural = "Activity Types"
+        verbose_name = 'Tipo de atividade'
+        verbose_name_plural = "Tipos de atividades"
 
 
 #
 # Professor
 #
 class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    course = models.ForeignKey(Course)
-    title = models.ForeignKey(Title)
-    area = models.ManyToManyField(Area)
-    contract_type = models.ForeignKey(ContractType)
-    phone1 = models.CharField(max_length=14)
-    phone2 = models.CharField(max_length=14)
-    email = models.EmailField()
-    active = models.BooleanField()
+    name = models.CharField('nome', max_length=80)
+    course = models.ForeignKey('curso', Course)
+    title = models.ForeignKey('títulação', Title)
+    area = models.ManyToManyField('área', Area)
+    contract_type = models.ForeignKey('tipo de contrato', ContractType)
+    phone1 = models.CharField('telefone 1', max_length=14)
+    phone2 = models.CharField('telefone 2', max_length=14)
+    email = models.EmailField('e-mail')
+    active = models.BooleanField('ativo')
     efetivo = models.BooleanField()
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = "Teachers"
+        verbose_name = 'Professor(a)'
+        verbose_name_plural = "Professores"
 
 
 #
 # Atividade
 #
 class Activity(models.Model):
-    teacher = models.ForeignKey(Teacher)
-    act_type = models.ForeignKey(ActivityType)
-    quantity = models.IntegerField()
-    date_ini = models.DateField()
-    date_term = models.DateField()
-    observations = models.TextField()
+    teacher = models.ForeignKey('professor(a)', Teacher)
+    act_type = models.ForeignKey('tipo de atividade', ActivityType)
+    quantity = models.IntegerField('quantidade')
+    date_ini = models.DateField('data inicial')
+    date_term = models.DateField('data final')
+    observations = models.TextField('observações')
 
     def __unicode__(self):
         return self.nome
+
     class Meta:
-        verbose_name_plural = "Activities"
+        verbose_name = 'Atividade'
+        verbose_name_plural = "Atividades"
 
 """
 class User(models.Model):
