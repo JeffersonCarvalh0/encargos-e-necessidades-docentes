279608e187148b9e7eee80a578d9246444fc8fcb
Athyrson Machado Ribeiro <athyrsomr@gmail.com>
2018-01-19 15:10:22 -0300

    adicionado campo ordering em admin.py

--- sistema/crud/admin.py
+++ sistema/crud/admin.py
@@ -26,57 +26,70 @@ AdminSite.site_url = None
 
 @admin.register(Campus)
 class CampusAdmin(admin.ModelAdmin):
-    ordering = ('name',)
+    ordering = ('short_name',)
     list_display = ('short_name', 'add_city', 'phone1', 'email', 'active', )
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'short_name', 'campus', 'active', )
 
 @admin.register(CourseGrid)
 class CourseGridAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'course', 'date_ini', 'date_term', 'active',)
 
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name',)
 
 @admin.register(Discipline)
 class DisciplineAdmin(admin.ModelAdmin):
+    ordering = ('short_name',)
     list_display = ('short_name', 'grid', 'area', 'block', 'work_load',)
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name',)
 
 @admin.register(ContractType)
 class ContractTypeAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'wk_teaching', 'wk_resext', 'wk_compl', 'active',)
 
 @admin.register(ActivityNature)
 class ActivityNatureAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name',)
 
 @admin.register(ActivityType)
 class ActivityTypeAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'wk_week', 'wk_limit', 'nature',)
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'course', 'title', 'efetivo', 'active')
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('act_type', 'date_ini', 'date_term',)
 
 @admin.register(Semester)
 class SemesterAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'date_ini', 'date_term',)
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
+    ordering = ('name',)
     list_display = ('name', 'semester', 'course', 'date_ini', 'date_term',)
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
+    ordering = ('offer',)
     list_display = ('offer', 'discipline', 'teacher',)
