e6b5b32c2845febc76b09b3b8e986f1c63043030
Athyrson Machado Ribeiro <athyrsonmr@gmail.com>
2018-01-21 21:23:53 -0300

    Revert "clonando"
    This reverts commit 77c06cf37816356673373b2da8869b3afc70214f, reversing
    changes made to 14a35e9ba57eb3720da2303ed697bc0b987fb067.

--- sistema/crud/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'crud.apps.CrudConfig'
--- sistema/crud/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-from crud.models import Campus
-
-class CampusForm(forms.ModelForm):
-    class Meta:
-        model = Campus
-        fields = '__all__'
