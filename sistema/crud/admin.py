23ad05b345d67ebe7e50f9565e02407aa3f261e3
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-22 22:23:21 -0300

    Tabular inlines adicionados, template sobrescrito para aparecer o link pro form do modelo no primeiro campo

--- sistema/crud/admin.py
+++ sistema/crud/admin.py
@@ -24,25 +24,65 @@ AdminSite.index_title = 'UESPI'
 AdminSite.site_title = 'Controle de Encargos Docentes'
 AdminSite.site_url = None
 
+# Some inline definitions for the admin
+class CourseInline(admin.TabularInline):
+    model = Course
+    show_change_link = True
+    extra = 0
+
+class DisciplineInline(admin.TabularInline):
+    model = Discipline
+    show_change_link = True
+    extra = 0
+
+class TeacherInline(admin.TabularInline):
+    model = Teacher
+    show_change_link = True
+    extra = 0
+
+class ActivityInline(admin.TabularInline):
+    model = Activity
+    show_change_link = True
+    extra = 0
+
+class ActivityTypeInline(admin.TabularInline):
+    model = Activity
+    show_change_link = True
+    extra = 0
+
+class EnrollmentInline(admin.TabularInline):
+    model = Enrollment
+    show_change_link = True
+    extra = 0
+
+class OfferInline(admin.TabularInline):
+    model = Offer
+    show_change_link = True
+    extra = 0
+
 @admin.register(Campus)
 class CampusAdmin(admin.ModelAdmin):
     ordering = ('short_name',)
     list_display = ('short_name', 'add_city', 'phone1', 'email', 'active', )
+    inlines = [CourseInline,]
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'short_name', 'campus', 'active', )
+    inlines = [TeacherInline,]
 
 @admin.register(CourseGrid)
 class CourseGridAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'course', 'date_ini', 'date_term', 'active',)
+    inlines = [DisciplineInline,]
 
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name',)
+    inlines = [DisciplineInline,]
 
 @admin.register(Discipline)
 class DisciplineAdmin(admin.ModelAdmin):
@@ -53,11 +93,13 @@ class DisciplineAdmin(admin.ModelAdmin):
 class TitleAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name',)
+    inlines = [TeacherInline,]
 
 @admin.register(ContractType)
 class ContractTypeAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'wk_teaching', 'wk_resext', 'wk_compl', 'active',)
+    inlines = [TeacherInline,]
 
 @admin.register(ActivityNature)
 class ActivityNatureAdmin(admin.ModelAdmin):
@@ -68,6 +110,7 @@ class ActivityNatureAdmin(admin.ModelAdmin):
 class ActivityTypeAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'wk_week', 'wk_limit', 'nature',)
+    inlines = [ActivityInline,]
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
@@ -83,11 +126,13 @@ class ActivityAdmin(admin.ModelAdmin):
 class SemesterAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'date_ini', 'date_term',)
+    inlines = [OfferInline,]
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
     ordering = ('name',)
     list_display = ('name', 'semester', 'course', 'date_ini', 'date_term',)
+    inlines = [EnrollmentInline,]
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
